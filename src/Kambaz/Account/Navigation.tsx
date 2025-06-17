c7825123072bd56faa1624d3cf5a0dfcfaace069
RyanFWebb <rfwebb24@gmail.com>
2025-06-16 22:58:51 -0400

    a6 database

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -4,8 +4,17 @@ import { ListGroup } from "react-bootstrap";
 
 export default function CourseNavigation() {
     const { currentUser } = useSelector((state: any) => state.accountReducer);
-    const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+    let links;
+    if (currentUser && currentUser.role === "ADMIN") {
+        links = ["Profile", "Users"];
+    } else if (currentUser) {
+        links = ["Profile"];
+    } else {
+        links = ["Signin", "Signup"];
+    }
+    // const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
     const { pathname } = useLocation();
+    // const active = (path: string) => (pathname.includes(path) ? "active" : "");
 
     return (
         <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
@@ -46,6 +55,8 @@ export default function CourseNavigation() {
                     );
                 })}
             </ListGroup>
+            {/* {currentUser && currentUser.role === "ADMIN" && (
+                <Link to={`/Kambaz/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link> )} */}
         </div>
     );
 }
