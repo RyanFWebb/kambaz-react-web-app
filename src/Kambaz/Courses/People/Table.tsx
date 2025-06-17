1154cacdb58392226590db0cb733d2b15b2dff5c
RyanFWebb <rfwebb24@gmail.com>
2025-06-17 18:10:30 -0400

    Updated people table

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,44 +1,65 @@
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
-// import { useState, useEffect } from "react";
-// import * as client from "../../Account/client";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import PeopleDetails from "./Details";
-// import { useParams } from "react-router-dom";
-// import * as db from "../../Database";
-export default function PeopleTable({ users = [] }: { users?: any[] }) {
-    // const { cid } = useParams();
-    // const { users, enrollments } = db;
-    return (
-        <div id="wd-people-table">
-            <PeopleDetails />
-            <Table striped>
-                <thead>
-                    <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
-                </thead>
-                <tbody>
-                    {users
-                        // .filter((usr) =>
-                        // enrollments.some((enrollment) => enrollment.user === usr._id && enrollment.course === cid)
-                        // )
-                        .map((user: any) => (
-                            <tr key={user._id}>
-                                <td className="wd-full-name text-nowrap">
-                                    <Link to={`/Kambaz/Account/Users/${user._id}`} className="text-decoration-none red-font">
-                                        <FaUserCircle className="me-2 fs-1 text-secondary" />
-                                        <span className="wd-first-name">{user.firstName}</span>
-                                        <span className="wd-last-name">{user.lastName}</span>
-                                    </Link>
-                                </td>
-                                <td className="wd-login-id">{user.loginId}</td>
-                                <td className="wd-section">{user.section}</td>
-                                <td className="wd-role">{user.role}</td>
-                                <td className="wd-last-activity">{user.lastActivity}</td>
-                                <td className="wd-total-activity">{user.totalActivity}</td>
-                            </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
-    );
+import * as courseClient from "../../Courses/client"; // Make sure path is correct
+
+export default function PeopleTable() {
+  const { cid } = useParams();
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      try {
+        if (cid) {
+          const enrolledUsers = await courseClient.findUsersForCourse(cid);
+          setUsers(enrolledUsers);
+        }
+      } catch (err) {
+        console.error("Failed to fetch users for course:", err);
+      }
+    };
+
+    loadUsers();
+  }, [cid]);
+
+  return (
+    <div id="wd-people-table">
+      <PeopleDetails />
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user: any) => (
+            <tr key={user._id}>
+              <td className="wd-full-name text-nowrap">
+                <Link
+                  to={`/Kambaz/Account/Users/${user._id}`}
+                  className="text-decoration-none red-font"
+                >
+                  <FaUserCircle className="me-2 fs-1 text-secondary" />
+                  <span className="wd-first-name">{user.firstName}</span>
+                  <span className="wd-last-name ms-1">{user.lastName}</span>
+                </Link>
+              </td>
+              <td className="wd-login-id">{user.loginId}</td>
+              <td className="wd-section">{user.section}</td>
+              <td className="wd-role">{user.role}</td>
+              <td className="wd-last-activity">{user.lastActivity}</td>
+              <td className="wd-total-activity">{user.totalActivity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
