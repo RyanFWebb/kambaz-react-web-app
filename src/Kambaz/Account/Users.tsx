3c7d5829ccacb4f35c1de1acb6d4a0564eddd67c
RyanFWebb <rfwebb24@gmail.com>
2025-06-17 18:13:40 -0400

    People table fix

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -83,7 +83,7 @@ export default function Users() {
                 <option value="FACULTY">Faculty</option>
                 <option value="ADMIN">Administrators</option>
             </select>
-            <PeopleTable users={users} />
+            <PeopleTable />
         </div>
     );
 }
