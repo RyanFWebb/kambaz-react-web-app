18c63c305aab89aa3e9afd24f7645a2d0d7bd885
RyanFWebb <rfwebb24@gmail.com>
2025-06-20 00:01:44 -0400

    add quiz bug fix

--- src/Kambaz/Courses/Quizzes/client.ts
+++ src/Kambaz/Courses/Quizzes/client.ts
@@ -6,27 +6,27 @@ const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`;
 
 export const fetchQuizzesForCourse = async (cid: string) => {
-  const response = await axiosWithCredentials.get(`${COURSES_API}/${cid}/quizzes`);
-  console.log('API response data:', response.data);
-  return response.data;
+    const response = await axiosWithCredentials.get(`${COURSES_API}/${cid}/quizzes`);
+    console.log('API response data:', response.data);
+    return response.data;
 };
 
 export const fetchQuizById = async (qid: string) => {
-  const response = await axiosWithCredentials.get(`${QUIZZES_API}/${qid}`);
-  return response.data;
+    const response = await axiosWithCredentials.get(`${QUIZZES_API}/${qid}`);
+    return response.data;
 };
 
-export const createQuiz = async (qid: string, quiz: any) => {
-  const response = await axiosWithCredentials.post(`${COURSES_API}/${qid}/quizzes`, quiz);
-  return response.data;
+export const createQuiz = async (cid: string, quiz: any) => {
+    const response = await axiosWithCredentials.post(`${COURSES_API}/${cid}/quizzes`, quiz);
+    return response.data;
 };
 
 export const updateQuiz = async (qid: string, updates: any) => {
-  const response = await axiosWithCredentials.put(`${QUIZZES_API}/${qid}`, updates);
-  return response.data;
+    const response = await axiosWithCredentials.put(`${QUIZZES_API}/${qid}`, updates);
+    return response.data;
 };
 
 export const deleteQuiz = async (qid: string) => {
-  const response = await axiosWithCredentials.delete(`${QUIZZES_API}/${qid}`);
-  return response.status === 200;
+    const response = await axiosWithCredentials.delete(`${QUIZZES_API}/${qid}`);
+    return response.status === 200;
 };
